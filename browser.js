9d698b4f1ce92f71771f36547caa45963f337bbd
Luan Nguyen <hieuluan.2109@gmail.com>
2023-08-30 23:48:25 +0700

    feat: add prettier config

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 const startBrowser = async () => {
   let browser;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import puppeteer from 'puppeteer';
-import dotenv from 'dotenv';
-import startBrowser from './browser.js';
+import puppeteer from "puppeteer";
+import dotenv from "dotenv";
+import startBrowser from "./browser.js";
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -10,34 +10,38 @@ const main = async () => {
 
   try {
     for (const url of urls) {
-      console.log('Start crawling ', url);
+      console.log("Start crawling ", url);
       const browser = await startBrowser();
       const page = await browser.newPage();
       await page.goto(url);
 
-      await page.waitForSelector('.directory-list')
-      const ulElement = await page.$('.directory-list');
+      await page.waitForSelector(".directory-list");
+      const ulElement = await page.$(".directory-list");
 
       if (ulElement) {
-        const providerElements = await ulElement.$$('.provider');
+        const providerElements = await ulElement.$$(".provider");
 
         for (const providerElement of providerElements) {
-          const aElement = await providerElement.$('.company_title.directory_profile');
+          const aElement = await providerElement.$(
+            ".company_title.directory_profile",
+          );
 
           if (aElement) {
-            const companyName = await aElement.evaluate(element => element.textContent.trim());
+            const companyName = await aElement.evaluate((element) =>
+              element.textContent.trim(),
+            );
             console.log(companyName);
           }
         }
       } else {
-        console.log('UL element not found');
+        console.log("UL element not found");
       }
-      
+
       await page.close();
       await browser.close();
     }
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error("An error occurred:", error);
   }
 };
 
