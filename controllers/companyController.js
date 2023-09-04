4c968cb4e9da5c51fcbb76e5ad1a4c393224528c
Luan Nguyen <hieuluan.2109@gmail.com>
2023-09-04 20:57:02 +0700

    feat: add client name to company data

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -14,20 +14,13 @@ const handleCrawlingCompany = async (page) => {
 
     for (let i = 0; i < articles.length; i++) {
       const article = articles[i];
-  
-      const locationData = await page.evaluate(element => {
-        const liElement = element.querySelector('li[data-tooltip-content="<i>Location</i>"]');
-        if (liElement) {
-          const titleElement = liElement.querySelector('.reviewer_list__details-title.sg-text__title');
-          return titleElement ? titleElement.textContent : null;
-        }
-        return null;
-      }, article);
-  
-      if (locationData) {
-        companyData[i] = {
-          location: locationData
-        }
+
+      const clientName = await getClientName(page, article);
+      const locationData = await getClientLocation(page, article);
+
+      companyData[i] = {
+        location: locationData,
+        clietnName: clientName
       }
     }
   } else {
@@ -36,5 +29,38 @@ const handleCrawlingCompany = async (page) => {
 
   return companyData;
 };
+
+
+/**
+ * @param {Page} page
+ * @param {ElementHandle<HTMLElement>} article
+ * @returns {string} clientLocation
+ */
+const getClientLocation =  async (page, article) => {
+   return await page.evaluate(element => {
+    const liElement = element.querySelector('li[data-tooltip-content="<i>Location</i>"]');
+
+    if (liElement) {
+      const titleElement = liElement.querySelector('.reviewer_list__details-title.sg-text__title');
+
+      return titleElement ? titleElement.textContent : '';
+    }
+
+    return null;
+  }, article);
+};
+
+/**
+ * @param {Page} page
+ * @param {ElementHandle<HTMLElement>} article
+ * @returns {string} clientName
+ */
+const getClientName = async (page, article) => {
+  return await page.evaluate(element => {
+    const h4Element = element.querySelector('.profile-review__header > h4');
+
+    return h4Element ? h4Element.textContent : '';
+  }, article);
+}
 
 export default handleCrawlingCompany;
